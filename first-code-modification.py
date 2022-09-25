2ca22ec080f74e4919aaf0a35bb7127fd1e8baf4
bc10201 <bmym2010xh2@gmail.com>
2022-09-25 15:54:15 +0800

    省略作画过程 提示 代码完成

--- first-code-modification.py
+++ first-code-modification.py
@@ -1,4 +1,8 @@
 import turtle
+if (input("省略作画过程？（是或不是）")=="是"):
+	turtle.tracer(False)
+else:
+	print("耐心")
 penColorList = ["red","blue","yellow","green"]
 pen = turtle.Pen()
 pen.speed(0)
@@ -13,4 +17,5 @@ pen.goto(-200,-200)
 pen.pd()
 pen.pencolor("black")
 pen.write("欲买桂花同载酒，终不似，少年时",font=("宋体",12,"normal"))
+print("作画完成")
 turtle.done()
