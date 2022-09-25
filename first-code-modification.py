c357ba986ea51c6dcfd2c11bf7edfc9acb08fc54
bc10201 <bmym2010xh2@gmail.com>
2022-09-25 15:49:42 +0800

    更改长度

--- first-code-modification.py
+++ first-code-modification.py
@@ -3,9 +3,9 @@ penColorList = ["red","blue","yellow","green"]
 pen = turtle.Pen()
 pen.speed(0)
 
-for i in range(500):
+for i in range(2500):
 	pen.pencolor(penColorList[i%4])
-	pen.forward(i)
+	pen.forward(i/10)
 	pen.left(100)
 	pen.hideturtle()
 turtle.done()
